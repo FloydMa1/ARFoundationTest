2ad5d331a344c4d012c7b062b6f58dd76c467a85
FloydMa1 <f.lehman@ziggo.nl>
2019-02-26 16:48:26 +0100

    Startup of Test

---
+++ ARFoundation tut/Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject placementInd;
+
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private bool poseIsValid = false;
+
+    void Start()
+    {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+    }
+    
+
+    void Update()
+    {
+        UpdatePlacement();
+        UpdatePlacementInd();
+    }
+
+    private void UpdatePlacementInd()
+    {
+        placementInd.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+    }
+
+    private void UpdatePlacement()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arOrigin.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        poseIsValid = hits.Count > 0;
+
+        if (poseIsValid)
+        {
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.current.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0, cameraForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+}
